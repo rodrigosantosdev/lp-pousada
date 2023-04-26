33651e9460bef7e3c5c3e7d00bd48e363fe950f4
rodrigosantosdev <rodrigofrontend@gmail.com>
2023-04-26 00:12:48 -0300

    geralzao

---
+++ scripts/app.js
@@ -0,0 +1,6 @@
+const buttonMenu = document.querySelector("#buttonMenu");
+const menuActive = document.querySelector("#menuActive");
+
+buttonMenu.addEventListener("click", () => {
+  menuActive.classList.toggle("active");
+});
